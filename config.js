9814ee10f3b02dbe74dbaaa068819a1e7173798c
Speedy58200 <lesasserts58@gmail.com>
2026-06-29 19:26:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-
 const CONFIG = {
-  apiKey: "79b2aebdd19252e094d44a808bfb7b59",
-  lat: 47.2454,
-  lon: 2.5539,
-  ville: "Cosne-sur-Loire",
-  units: "metric",
-  lang: "fr"
+    apiKey: "79b2aebdd19252e094d44a808bfb7b59",
+
+    lat: 47.4120,
+    lon: 2.9253,
+
+    ville: "Cosne-sur-Loire",
+
+    units: "metric",
+    lang: "fr"
 };
+  
